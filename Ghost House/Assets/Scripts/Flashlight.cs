062bb94858bcf026a24fee20943489b93102ab3e
Unknown <nathan.o798@stu.nebo.edu>
2017-10-25 13:10:02 -0600

    Worked on layout and flashlight on/off script (need to finish)

---
+++ Ghost House/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Flashlight : MonoBehaviour {
+
+	public bool lightOn;
+
+	Light light;
+
+	void Start () {
+		light = GetComponent<Light> ();
+		//Set Light default to On
+		lightOn = true;
+		light.enabled = true;
+	}
+	
+	void Update () {
+		//Toggle light on/off when E key is pressed
+		if (Input.GetKeyUp (KeyCode.E) && lightOn) {
+			lightOn = false;
+			light.enabled = false;
+		}
+
+		else if (Input.GetKeyUp (KeyCode.E) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
+		}
+	}
+}
