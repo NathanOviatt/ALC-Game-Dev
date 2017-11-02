2b7593951812071b06a55868ef84e53fd1f48f48
Unknown <nathan.o798@stu.nebo.edu>
2017-11-02 13:08:47 -0600

    Flashlight stun script started, outside steps started(need to finish)

---
+++ Ghost House/Assets/Scripts/GhostStun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostStun : MonoBehaviour {
+
+	//bool lightCheck;
+
+	void Start () {
+		//lightCheck = GetComponent<Flashlight>().lightOn;
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.gameObject.name == "Ghost"){
+			print("Ghost is stunned!");
+		}
+	}
+}
